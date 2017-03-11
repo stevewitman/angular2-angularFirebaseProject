18c003096c529e92d5b8f421b7ea99acd0c0b9b1
Steve Witman <stevewitman@gmail.com>
2017-03-10 23:30:45 -0700

    Set up material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router'
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomePageComponent },
+  { path: '', component: LoginPageComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes),
+    MaterialModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
